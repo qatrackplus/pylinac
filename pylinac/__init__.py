5886fd44f998c074be7a7f4d9fa6899cef690a26
randlet <randle.taylor@gmail.com>
2020-11-23 16:03:39 -0500

    bump version

--- pylinac/__init__.py
+++ pylinac/__init__.py
@@ -1,8 +1,8 @@
 
 import sys
 
-__version__ = '2.3.1.6'
-__version_info__ = (2, 3, 1, 6)
+__version__ = '2.3.1.7'
+__version_info__ = (2, 3, 1, 7)
 
 # check python version
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
