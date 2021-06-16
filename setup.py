f95dedd700b2efe5478317a7fbafef8dd09bf54d
randlet <randle.taylor@gmail.com>
2021-06-16 16:07:15 -0400

    don't import version so doesn't fail on missing cached_property on Python 3.6

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-from pylinac import __version__
+# from pylinac import __version__
+__version__ =  '3.0rc1'
 
 
 setup(
